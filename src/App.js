580022051b13d3dd3938a7fc964326e585974b2a
zeeracha <zeeraratcha@gmail.com>
2023-01-10 01:52:13 +0700

    Dom ProtectRoute

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Routes,
   Link,
   BrowserRouter,
+  Navigate,
 } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
@@ -17,11 +18,20 @@ function App() {
 
   const {currentUser} = useContext(AuthContext)
   // console.log(currentUser)
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      return <Navigate to="/login"/>;
+    }
+  }
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/">
-        <Route index element={<Home/>}/>
+        <Route index element={
+          <ProtectedRoute>
+            <Home/>
+          </ProtectedRoute>}/>
         <Route path="login" element={<Login/>}/>
         <Route path="register" element={<Register/>}/>
       </Route>
