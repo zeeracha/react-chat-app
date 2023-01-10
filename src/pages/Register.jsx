10e2203fd99ace0d45c9c29ae288971f13b1099f
zeeracha <zeeraratcha@gmail.com>
2023-01-11 00:52:41 +0700

    corrected Login Email and Password

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,19 @@
-import Add from "../img/addAvatar.png" 
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
-import { useState } from 'react';
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"; 
-import { useNavigate, Link } from 'react-router-dom';
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
-function Register() {
-
+const Register = () => {
   const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
     // console.log(e.target[0].value)
     const displayName = e.target[0].value;
     const email = e.target[1].value;
@@ -20,61 +21,69 @@ function Register() {
     const file = e.target[3].files[0];
 
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password)
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, displayName);
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      // Register three observers
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        }, 
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
             });
-            await setDoc(doc(db, "user", res.user.uid),{
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
               email,
               photoURL: downloadURL,
             });
 
+            //create empty user chats on firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
             navigate("/");
-          });
-        }
-      );
-    }catch(err){
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
       setErr(true);
-    } 
-  }
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
+    <div className="formContainer">
+      <div className="formWrapper">
         <span className="logo">Zeebrrra Chat</span>
         <span className="title">Register</span>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="display name"/>
-                <input type="email" placeholder="email"/>
-                <input type="password" placeholder="password"/>
-                <input style={{display:"none"}} type="file" id="file"/>
-                <label htmlFor="file">
-                    <img src={Add} alt="" />
-                    <span>Add an avartar</span>
-                </label>
-                <button>Sign up</button>
-                {err && <span>Something went wrong</span>}
-            </form>
-            <p>You do have an account  <Link to="/login">Login</Link></p>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
